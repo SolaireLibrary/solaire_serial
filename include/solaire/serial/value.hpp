c8fd10de628c30fa6d3057c85934e7e3475a0ea0
SolaireLibrary <solairelibrary@mail.com>
2016-05-12 22:04:08 +0100

    Added partial implementation of value

---
+++ include/solaire/serial/value.hpp
@@ -0,0 +1,104 @@
+#ifndef SOLAIRE_SERIAL_VALUE_HPP
+#define SOLAIRE_SERIAL_VALUE_HPP
+
+//Copyright 2016 Adam G. Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http ://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#include <map>
+#include <vector>
+#include "solaire/serial/serial.hpp"
+
+//! \todo Use exportable classes instead of STL
+
+namespace solaire { namespace serial {
+	class value {
+	protected:
+		union {
+			char mChar;
+			bool mBool;
+			uint64_t mUnsigned;
+			int64_t mSigned;
+			double mFloat;
+			void* mPointer;
+		};
+		uint8_t mType;
+	public:
+		value();
+		value(value&&);
+		value(const value&);
+		value(char);
+		value(bool);
+		value(uint8_t);
+		value(uint16_t);
+		value(uint32_t);
+		value(uint64_t);
+		value(int8_t);
+		value(int16_t);
+		value(int32_t);
+		value(int64_t);
+		value(float);
+		value(double);
+		value(const std::string&);
+		value(const std::vector<value>&);
+		value(const std::map<std::string, value>&);
+		~value();
+
+		value& operator=(value&&);
+		value& operator=(const value&);
+
+		bool is_void() const throw();
+		bool is_char() const throw();
+		bool is_bool() const throw();
+		bool is_unsigned() const throw();
+		bool is_signed() const throw();
+		bool is_float() const throw();
+		bool is_pointer() const throw();
+		bool is_string() const throw();
+		bool is_array() const throw();
+		bool is_object() const throw();
+
+		char get_char() const;
+		bool get_bool() const;
+		uint64_t get_unsigned() const;
+		int64_t get_signed() const;
+		double get_float() const;
+		void* get_pointer() const;
+		std::string get_string() const;
+		std::vector<value> get_array() const;
+		std::map<std::string, value> get_object() const;
+
+		char& get_char();
+		bool& get_bool();
+		uint64_t& get_unsigned();
+		int64_t& get_signed();
+		double& get_float();
+		void*& get_pointer();
+		std::string& get_string();
+		std::vector<value>& get_array();
+		std::map<std::string, value>& get_object();
+
+		void set_void();
+		void set_char(char);
+		void set_bool(bool);
+		void set_unsigned(uint64_t);
+		void set_signed(int64_t);
+		void set_float(double);
+		void set_pointer(void*);
+		void set_string(const std::string&);
+		void set_array(const std::vector<value>&);
+		void set_object(const std::map<std::string, value>&);
+	};
+}}
+
+#endif
---
+++ src/solaire/serial/value.cpp
@@ -0,0 +1,449 @@
+//Copyright 2016 Adam G. Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http ://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#include "solaire/core/interfaces/allocator.hpp"
+#include "solaire/serial/value.hpp"
+
+enum {
+	VOID_T,
+	CHAR_T,
+	BOOL_T,
+	UNSIGNED_T,
+	SIGNED_T,
+	FLOAT_T,
+	POINTER_T,
+	STRING_T,
+	ARRAY_T,
+	OBJECT_T
+};
+
+namespace solaire { namespace serial {
+	// value
+	value::value() :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{}
+
+	value::value(value&& aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		operator=(std::move(aValue));
+	}
+
+	value::value(const value& aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		operator=(aValue);
+	}
+
+	value::value(char aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_char(aValue);
+	}
+
+	value::value(bool aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_char(aValue);
+	}
+
+	value::value(uint8_t aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_unsigned(aValue);
+	}
+
+	value::value(uint16_t aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_unsigned(aValue);
+	}
+
+	value::value(uint32_t aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_unsigned(aValue);
+	}
+
+	value::value(uint64_t aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_unsigned(aValue);
+	}
+
+	value::value(int8_t aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_signed(aValue);
+	}
+
+	value::value(int16_t aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_signed(aValue);
+	}
+
+	value::value(int32_t aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_signed(aValue);
+	}
+
+	value::value(int64_t aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_signed(aValue);
+	}
+
+	value::value(float aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_float(aValue);
+	}
+
+	value::value(double aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_float(aValue);
+	}
+
+	value::value(const std::string& aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_string(aValue);
+	}
+
+	value::value(const std::vector<value>& aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_array(aValue);
+	}
+
+	value::value(const std::map<std::string, value>& aValue) :
+		mPointer(nullptr),
+		mType(VOID_T)
+	{
+		set_object(aValue);
+	}
+
+	value::~value() {
+		set_void();
+	}
+
+	value& value::operator=(value&& aOther) {
+		set_void();
+		switch(aOther.mType)
+		{
+		case CHAR_T:
+			mChar = aOther.mChar;
+			break;
+		case BOOL_T:
+			mBool = aOther.mBool;
+			break;
+		case UNSIGNED_T:
+			mUnsigned = aOther.mUnsigned;
+			break;
+		case SIGNED_T:
+			mSigned = aOther.mSigned;
+			break;
+		case FLOAT_T:
+			mFloat = aOther.mFloat;
+			break;
+		case POINTER_T:
+		case STRING_T:
+		case ARRAY_T:
+		case OBJECT_T:
+			mPointer = aOther.mPointer;
+			break;
+		default:
+			break;
+		}
+		mType = aOther.mType;
+		aOther.mPointer = nullptr;
+		aOther.mType = VOID_T;
+		return *this;
+	}
+
+	value& value::operator=(const value& aOther) {
+		switch (aOther.mType)
+		{
+		case CHAR_T:
+			set_char(aOther.mChar);
+			break;
+		case BOOL_T:
+			set_bool(aOther.mBool);
+			break;
+		case UNSIGNED_T:
+			set_unsigned(aOther.mUnsigned);
+			break;
+		case SIGNED_T:
+			set_signed(aOther.mSigned);
+			break;
+		case FLOAT_T:
+			set_float(aOther.mFloat);
+			break;
+		case POINTER_T:
+			set_pointer(aOther.mPointer);
+			break;
+		case STRING_T:
+			set_string(*static_cast<const std::string*>(aOther.mPointer));
+			break;
+		case ARRAY_T:
+			set_array(*static_cast<const std::vector<value>*>(aOther.mPointer));
+			break;
+		case OBJECT_T:
+			set_object(*static_cast<const std::map<std::string, value>*>(aOther.mPointer));
+			break;
+		default:
+			break;
+		}
+		return *this;
+	}
+
+	bool value::is_void() const throw() {
+		return mType == VOID_T;
+	}
+
+	bool value::is_char() const throw() {
+		return false;
+	}
+
+	bool value::is_bool() const throw() {
+		return false;
+	}
+
+	bool value::is_unsigned() const throw() {
+		return false;
+	}
+
+	bool value::is_signed() const throw() {
+		return false;
+	}
+
+	bool value::is_float() const throw() {
+		return false;
+	}
+
+	bool value::is_pointer() const throw() {
+		return false;
+	}
+
+	bool value::is_string() const throw() {
+		return false;
+	}
+
+	bool value::is_array() const throw() {
+		return false;
+	}
+
+	bool value::is_object() const throw() {
+		return false;
+	}
+
+	char value::get_char() const {
+		throw std::runtime_error("solaire::serial::value::get_char : Value is not convertable to char");
+	}
+
+	bool value::get_bool() const {
+		throw std::runtime_error("solaire::serial::value::get_bool : Value is not convertable to bool");
+	}
+
+	uint64_t value::get_unsigned() const {
+		throw std::runtime_error("solaire::serial::value::get_unsigned : Value is not convertable to unsigned");
+	}
+
+	int64_t value::get_signed() const {
+		throw std::runtime_error("solaire::serial::value::get_signed : Value is not convertable to signed");
+	}
+
+	double value::get_float() const {
+		throw std::runtime_error("solaire::serial::value::get_float : Value is not convertable to float");
+	}
+
+	void* value::get_pointer() const {
+		throw std::runtime_error("solaire::serial::value::get_pointer : Value is not convertable to pointer");
+	}
+
+	std::string value::get_string() const {
+		throw std::runtime_error("solaire::serial::value::get_string : Value is not convertable to string");
+	}
+
+	std::vector<value> value::get_array() const {
+		throw std::runtime_error("solaire::serial::value::get_array : Value is not convertable to array");
+	}
+
+	std::map<std::string, value> value::get_object() const {
+		throw std::runtime_error("solaire::serial::value::get_object : Value is not convertable to object");
+	}
+
+	char& value::get_char() {
+		if(mType != CHAR_T) operator=(std::move(value(const_cast<const value*>(this)->get_char())));
+		return mChar;
+	}
+
+	bool& value::get_bool() {
+		if (mType != BOOL_T) operator=(std::move(value(const_cast<const value*>(this)->get_bool())));
+		return mBool;
+	}
+
+	uint64_t& value::get_unsigned() {
+		if (mType != UNSIGNED_T) operator=(std::move(value(const_cast<const value*>(this)->get_unsigned())));
+		return mUnsigned;
+	}
+
+	int64_t& value::get_signed() {
+		if (mType != SIGNED_T) operator=(std::move(value(const_cast<const value*>(this)->get_signed())));
+		return mSigned;
+	}
+
+	double& value::get_float() {
+		if (mType != FLOAT_T) operator=(std::move(value(const_cast<const value*>(this)->get_float())));
+		return mFloat;
+	}
+
+	void*& value::get_pointer() {
+		if (mType != POINTER_T) operator=(std::move(value(const_cast<const value*>(this)->get_pointer())));
+		return mPointer;
+	}
+
+	std::string& value::get_string() {
+		if (mType != STRING_T) operator=(std::move(value(const_cast<const value*>(this)->get_string())));
+		return *static_cast<std::string*>(mPointer);
+	}
+
+	std::vector<value>& value::get_array() {
+		if (mType != ARRAY_T) operator=(std::move(value(const_cast<const value*>(this)->get_array())));
+		return *static_cast<std::vector<value>*>(mPointer);
+	}
+
+	std::map<std::string, value>& value::get_object() {
+		if (mType != OBJECT_T) operator=(std::move(value(const_cast<const value*>(this)->get_object())));
+		return *static_cast<std::map<std::string, value>*>(mPointer);
+	}
+
+	void value::set_void() {
+		switch(mType)
+		{
+		case STRING_T:
+			{
+				std::string* const tmp = static_cast<std::string*>(mPointer);
+				tmp->~basic_string();
+				get_allocator().deallocate(tmp);
+			}
+			break;
+		case ARRAY_T:
+			{
+				std::vector<value>* const tmp = static_cast<std::vector<value>*>(mPointer);
+				tmp->~vector();
+				get_allocator().deallocate(tmp);
+			}
+			break;
+		case OBJECT_T:
+			{
+				std::map<std::string, value>* const tmp = static_cast<std::map<std::string, value>*>(mPointer);
+				tmp->~map();
+				get_allocator().deallocate(tmp);
+			}
+			break;
+		default:
+			mPointer = nullptr;
+			break;
+		}
+		mType = VOID_T;
+	}
+
+	void value::set_char(char aValue) {
+		set_void();
+		mChar = aValue;
+		mType = CHAR_T;
+	}
+
+	void value::set_bool(bool aValue) {
+		set_void();
+		mBool = aValue;
+		mType = BOOL_T;
+	}
+
+	void value::set_unsigned(uint64_t aValue) {
+		set_void();
+		mUnsigned = aValue;
+		mType = UNSIGNED_T;
+	}
+
+	void value::set_signed(int64_t aValue) {
+		set_void();
+		mSigned = aValue;
+		mType = SIGNED_T;
+	}
+
+	void value::set_float(double aValue) {
+		set_void();
+		mFloat = aValue;
+		mType = FLOAT_T;
+	}
+
+	void value::set_pointer(void* aValue) {
+		set_void();
+		mPointer = aValue;
+		mType = POINTER_T;
+	}
+
+	void value::set_string(const std::string& aValue) {
+		if(mType == STRING_T) {
+			*static_cast<std::string*>(mPointer) = aValue;
+		}else {
+			set_void();
+			mPointer = new(get_allocator().allocate(sizeof(std::string))) std::string(aValue);
+			mType = STRING_T;
+		}
+	}
+
+	void value::set_array(const std::vector<value>& aValue) {
+		if(mType == ARRAY_T) {
+			*static_cast<std::vector<value>*>(mPointer) = aValue;
+		}else {
+			set_void();
+			mPointer = new(get_allocator().allocate(sizeof(std::vector<value>))) std::vector<value>(aValue);
+			mType = ARRAY_T;
+		}
+	}
+
+	void value::set_object(const std::map<std::string, value>& aValue) {
+		if(mType == OBJECT_T) {
+			*static_cast<std::map<std::string, value>*>(mPointer) = aValue;
+		}else {
+			set_void();
+			mPointer = new(get_allocator().allocate(sizeof(std::map<std::string, value>))) std::map<std::string, value>(aValue);
+			mType = OBJECT_T;
+		}
+	}
+
+}}
